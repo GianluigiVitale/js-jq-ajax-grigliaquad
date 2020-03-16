055f0cc98c2b2db0afef58886c79ec471e3abe62
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-03-16 16:10:43 +0100

    added comments to html,css,js

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ Il numero ottenuto appare al centro del quadrato.
 */
 $(document).ready(function() {
 
-    $(document).on('click', '.quadrato', function() {
+    $(document).on('click', '.quadrato', function() {       // al click di .quadrato funzione ajax che genera un numero da 1 a 9
         $.ajax({
             context: this,
             url: 'https://flynn.boolean.careers/exercises/api/random/int',
             method: 'GET',
             success: function(data) {
-                var prova = $(this).children('p').text(data.response);
-                $(this).removeClass('yellow green');
-                if (data.response <= 5) {
+                $(this).children('p').text(data.response);  // associo a questo .quadrato cliccato il numero generato
+                $(this).removeClass('yellow green');        // elimino l'eventuale sfondo del quadrato
+                if (data.response <= 5) {                   // se il numero e' <= 5 sfondo giallo altrimenti verde
                     $(this).addClass('yellow');
                 } else {
                     $(this).addClass('green');
@@ -25,15 +25,12 @@ $(document).ready(function() {
         });
     });
 
-    $('button').click(function() {
-        $('.quadrato').removeClass('yellow green');
-        $('.quadrato p').empty()
-    });
 
-    // $(document).on('click', '.quadrato', function() {
-    //     var prova = $(this).children('p').text();
-    //     console.log(prova);
-    // });
+
+    $('button').click(function() {                  // al click del pulsante resetto la griglia (elimino il numero e lo sfondo di tutti i quadrati)
+        $('.quadrato').removeClass('yellow green');
+        $('.quadrato p').empty();
+    });
 
 
 
